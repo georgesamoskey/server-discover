924f820cc1620237126491e455d6f90e2cfd798b
georgesamoskey <georgeskatiera@gmail.com>
2024-03-15 10:22:29 +0100

    Le premier commit

---
+++ src/main/java/payallserverdiscover/asyst/AsystApplication.java
@@ -0,0 +1,15 @@
+package payallserverdiscover.asyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AsystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/payallserverdiscover/asyst/AsystApplicationTests.java
@@ -0,0 +1,13 @@
+package payallserverdiscover.asyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
